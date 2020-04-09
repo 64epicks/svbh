fcf7b7ac355afef441065e5b2c2260a3104cb612
64epicks <jacob_paul@outlook.com>
2020-04-10 00:01:18 +0200

    Enhanced navbar

--- js/site.js
+++ js/site.js
@@ -1,34 +1,31 @@
 
 var app = new Vue({
     el: '#app',
-    data : {
+    data: {
     },
 
     mounted: function () {
-        setTimeout(function()
-        {
-            document.title = pagename + ' | Svensk backhoppning';
-            $("#app").css("visibility", "visible").hide().fadeIn(800);
-            $("#load_symbol").fadeOut(600);
-        }, 500);
+        document.title = pagename + ' | Svensk backhoppning';
+        $("#app").css("visibility", "visible").hide().fadeIn(800);
+        $("#load_symbol").fadeOut(600);
     }
 });
 
-(function($) { 
-    $(function() { 
-      $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.navbar-dropdown').toggle();
-        $('.dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-      $('html').click(function() {
-        $('.navbar-dropdown').hide();
-      });
-      $('#navbar-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      $('#navbar-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-    }); 
-  })(jQuery);
+(function ($) {
+    $(function () {
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.navbar-dropdown').slideToggle(200);
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.navbar-dropdown').hide();
+        });
+        $('#navbar-toggle').click(function () {
+            $('nav ul').slideToggle(200);
+        });
+        $('#navbar-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+    });
+})(jQuery);
